8ab1ce318c890894ea15551287cf736b07fcdd07
irul edgesky <irulannaba@gmail.com>
2024-09-23 22:07:45 +0700

    Refactore blog.blade.php : renaming to articles and provides some dummy article using view

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">Article 1 Title</h2>
+      <div class="text-base text-gray-500">
+        <a href="#">Muhammad Yusuf Haidar Khairullah</a> | 9 September 2023
+      </div>
+      <p class="my-4 font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque, qui officiis dignissimos mollitia placeat error officia ipsa cumque veritatis nihil explicabo deserunt, vel nostrum obcaecati ipsam, voluptatum eius consequatur.</p>
+      <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">Article 2 Title</h2>
+      <div class="text-base text-gray-500">
+        <a href="#">Muhammad Yusuf Haidar Khairullah</a> | 10 September 2023
+      </div>
+      <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsam culpa laborum est cum quasi architecto sit, consequatur at eaque ipsum earum! Doloremque, voluptate voluptatibus laudantium officiis culpa molestiae non!</p>
+      <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+  </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['title'=> 'Home Page']);
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['title' => 'About','owner' => 'Muhammad Yusuf Haidar Khairullah']);
+    return view('about', ['title' => 'About', 'owner' => 'Muhammad Yusuf Haidar Khairullah']);
 });
 
-Route::get('/blog', function(){
-    return view('blog', ['title' => 'Articles']);
+Route::get('/articles', function () {
+    return view('articles', ['title' => 'Articles', 'Articles' => [
+        [
+            'title' => 'Article Title 1',
+            'author' => 'Muhammad Yusuf Haidar Khairullah',
+            'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque, qui officiis dignissimos mollitia placeat error officia ipsa cumque veritatis nihil explicabo deserunt, vel nostrum obcaecati ipsam, voluptatum eius consequatur.'
+        ],
+        [
+            'title' => 'Article Title 2',
+            'author' => 'Muhammad Yusuf Haidar Khairullah',
+            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsam culpa laborum est cum quasi architecto sit, consequatur at eaque ipsum earum! Doloremque, voluptate voluptatibus laudantium officiis culpa molestiae non!'
+        ]
+    ]]);
 });
-Route::get('/contact', function(){
+Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
