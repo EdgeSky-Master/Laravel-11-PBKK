56134864fe0f4e7821279ffd998cbc3a9609bf58
irul edgesky <irulannaba@gmail.com>
2024-09-23 22:35:35 +0700

    Refactor article and articles views
    - Renamed blog.blade.php to articles.blade.php
    - Added new article.blade.php view
    - Updated articles.blade.php to display a list of articles
    - Added link to individual article in articles.blade.php
    - Added owner information to the about page
    - Refactored navbar component to use custom blade component for navigation links
    - Fixed navigation header for mobile

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,19 +12,45 @@ Route::get('/about', function () {
 });
 
 Route::get('/articles', function () {
-    return view('articles', ['title' => 'Articles', 'Articles' => [
+    return view('articles', ['title' => 'Articles', 'articles' => [
         [
+            'id' => 1,
             'title' => 'Article Title 1',
             'author' => 'Muhammad Yusuf Haidar Khairullah',
             'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque, qui officiis dignissimos mollitia placeat error officia ipsa cumque veritatis nihil explicabo deserunt, vel nostrum obcaecati ipsam, voluptatum eius consequatur.'
         ],
         [
+            'id' => 2,
             'title' => 'Article Title 2',
             'author' => 'Muhammad Yusuf Haidar Khairullah',
             'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsam culpa laborum est cum quasi architecto sit, consequatur at eaque ipsum earum! Doloremque, voluptate voluptatibus laudantium officiis culpa molestiae non!'
         ]
     ]]);
 });
+
+Route::get('/articles/{id}', function ($id) {
+    $articles = [
+        [
+            'id' => 1,
+            'title' => 'Article Title 1',
+            'author' => 'Muhammad Yusuf Haidar Khairullah',
+            'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque, qui officiis dignissimos mollitia placeat error officia ipsa cumque veritatis nihil explicabo deserunt, vel nostrum obcaecati ipsam, voluptatum eius consequatur.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Article Title 2',
+            'author' => 'Muhammad Yusuf Haidar Khairullah',
+            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsam culpa laborum est cum quasi architecto sit, consequatur at eaque ipsum earum! Doloremque, voluptate voluptatibus laudantium officiis culpa molestiae non!'
+        ]
+    ];
+
+    $post = Arr::first($articles, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    return view('article', ['title' => 'single article', 'article' => $post]);
+});
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
