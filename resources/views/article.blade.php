cc0d02529d0e15a6740083e4bc65e983be2a4f1e
irul edgesky <irulannaba@gmail.com>
2024-09-24 21:35:16 +0700

    Refactor article.blade.php: Remove unnecessary code and fix article title styling

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,11 +1,8 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-
-    
-      
     
     <article class="py-8 max-w-screen-md border-b">
-      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">{{ $article ['title'] }}</h2>
+      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $article ['title'] }}</h2>
       <div class="text-base text-gray-500">
         <a href="#">{{ $article ['author'] }}</a> | 9 September 2023
       </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,30 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Articles
+{
+    public static function allArticle()
+    {
+        return
+            [
+                [
+                    'id' => 1,
+                    'slug' => 'article-title-1',
+                    'title' => 'Article Title 1',
+                    'author' => 'Muhammad Yusuf Haidar Khairullah',
+                    'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque, qui officiis dignissimos mollitia placeat error officia ipsa cumque veritatis nihil explicabo deserunt, vel nostrum obcaecati ipsam, voluptatum eius consequatur.'
+                ],
+                [
+                    'id' => 2,
+                    'slug' => 'article-title-2',
+                    'title' => 'Article Title 2',
+                    'author' => 'Muhammad Yusuf Haidar Khairullah',
+                    'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsam culpa laborum est cum quasi architecto sit, consequatur at eaque ipsum earum! Doloremque, voluptate voluptatibus laudantium officiis culpa molestiae non!'
+                ]
+            ];
+    }
+}
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
@@ -12,43 +36,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/articles', function () {
-    return view('articles', ['title' => 'Articles', 'articles' => [
-        [
-            'id' => 1,
-            'slug' => 'article-title-1',
-            'title' => 'Article Title 1',
-            'author' => 'Muhammad Yusuf Haidar Khairullah',
-            'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque, qui officiis dignissimos mollitia placeat error officia ipsa cumque veritatis nihil explicabo deserunt, vel nostrum obcaecati ipsam, voluptatum eius consequatur.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'article-title-2',
-            'title' => 'Article Title 2',
-            'author' => 'Muhammad Yusuf Haidar Khairullah',
-            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsam culpa laborum est cum quasi architecto sit, consequatur at eaque ipsum earum! Doloremque, voluptate voluptatibus laudantium officiis culpa molestiae non!'
-        ]
-    ]]);
+    return view('articles', ['title' => 'Articles', 'articles' => Articles::allArticle()]);
 });
 
 Route::get('/articles/{slug}', function ($slug) {
-    $articles = [
-        [
-            'id' => 1,
-            'slug' => 'article-title-1',
-            'title' => 'Article Title 1',
-            'author' => 'Muhammad Yusuf Haidar Khairullah',
-            'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque, qui officiis dignissimos mollitia placeat error officia ipsa cumque veritatis nihil explicabo deserunt, vel nostrum obcaecati ipsam, voluptatum eius consequatur.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'article-title-2',
-            'title' => 'Article Title 2',
-            'author' => 'Muhammad Yusuf Haidar Khairullah',
-            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsam culpa laborum est cum quasi architecto sit, consequatur at eaque ipsum earum! Doloremque, voluptate voluptatibus laudantium officiis culpa molestiae non!'
-        ]
-    ];
-
-    $post = Arr::first($articles, function ($post) use ($slug) {
+    $post = Arr::first(Articles::allArticle(), function ($post) use ($slug) {
         return $post['slug'] == $slug;
     });
 
