c39ca7d7b05d7357b7924e4708dba16f3196bd8d
irul edgesky <irulannaba@gmail.com>
2024-10-13 16:45:11 +0700

    Make separate seeder for category and user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
         ]);
+        Article::factory(100)->recycle([
+            //User::factory(10)->create(),
+            // Category::factory(5)->create()
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
